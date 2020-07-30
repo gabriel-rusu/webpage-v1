4d52af7df87bec749e9b7c6c42ddfa7f27047ead
Gabriel Rusu <rusugabriel13@gmail.com>
2020-07-30 21:26:04 +0300

    dynamic-type component is finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webpage';
+  delta = 200;
   theme = 'dark-theme-word'
+  words = ['Software Engineer', 'Student']
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { DynamicTypeComponent } from './dynamic-type/dynamic-type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    EducationComponent,
+    DynamicTypeComponent
   ],
   imports: [
     BrowserModule
