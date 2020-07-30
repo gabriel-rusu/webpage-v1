f0bcc2e787ef896ee436eabce61a1b4554f20711
Gabriel Rusu <rusugabriel13@gmail.com>
2020-07-30 22:06:27 +0300

    updated the first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   delta = 140;
   theme = 'dark-theme-word'
-  words = ['Software Engineer', 'Student']
+  words = ["Software Engineer", "System Engineering student", "Undergraduate Teaching Assistant", "Open Source Enthusiast"]
 }
