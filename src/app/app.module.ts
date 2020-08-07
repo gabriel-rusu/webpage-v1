9462dcdebf29a626947e1c546880fb972ea404d8
Gabriel Rusu <rusugabriel13@gmail.com>
2020-08-07 21:21:16 +0300

    updated the front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { EducationComponent } from './education/education.component';
 import { DynamicTypeComponent } from './components/dynamic-type/dynamic-type.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProjectService } from './services/project/project.service';
 import { TypingComponent } from './components/typing/typing.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ProjectsComponent} from './pages/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExperienceComponent,
-    ProjectsComponent,
     EducationComponent,
+    ProjectsComponent,
     DynamicTypeComponent,
     TypingComponent,
   ],
