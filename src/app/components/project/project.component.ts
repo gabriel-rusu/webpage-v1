bcb7898583ae5021e8331a54c242dfd937e59621
Gabriel Rusu <rusugabriel13@gmail.com>
2020-08-15 09:26:20 +0300

    modified the projects layout

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { Project } from '../../models/project.model'
 
 @Component({
@@ -9,6 +9,8 @@ import { Project } from '../../models/project.model'
 export class ProjectComponent implements OnInit {
   @Input()
   project: Project;
+
+  @HostBinding('attr.class') cssClasses = 'pad'
 
 
   constructor() { }
