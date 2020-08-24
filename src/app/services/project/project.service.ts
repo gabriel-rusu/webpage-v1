3490dd8f8f0188c4e87bf3dbb27338342e7d7846
Gabriel Rusu <rusugabriel13@gmail.com>
2020-08-24 18:23:57 +0300

    fixed the project service typo

--- src/app/services/project/project.service.ts
+++ src/app/services/project/project.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {Project } from '../../models/project.model'
+import {Project } from '../../models/project.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
-  githubRepos = "https://api.github.com/users/RusuGabriel/reposs";
+  githubRepos = 'https://api.github.com/users/RusuGabriel/repos';
 
   constructor(private http: HttpClient) { }
 
