dda29c6af6901bc1daa4087b30b767e0b2bf3f60
Gabriel Rusu <rusugabriel13@gmail.com>
2020-08-27 22:25:46 +0300

    updated the experience service

--- src/app/services/experience/experience.service.ts
+++ src/app/services/experience/experience.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Experience } from 'src/app/models/experience.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperienceService {
+  url: string = 'ceva';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getExperience(): Observable<Experience[]> {
+    return this.http.get<Experience[]>(this.url);
+  }
 }
