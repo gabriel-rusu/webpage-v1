34f822bcd30656e928d3346b8de1e06148910cb5
Gabriel Rusu <rusugabriel13@gmail.com>
2020-08-28 20:49:57 +0300

    update: redesigned the experience component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 import {Experience} from '../../models/experience.model';
 
 @Component({
@@ -9,9 +9,24 @@ import {Experience} from '../../models/experience.model';
 export class ExperienceComponent implements OnInit {
   @Input()
   experience: Experience;
-  constructor() { }
+  @HostBinding('attr.class') cssClasses = 'pad';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  getPeriodOfTime(): string {
+    return this.process(this.experience.startDate) + ' - ' + this.process(this.experience.endDate);
+  }
+
+  process(date: Date): string {
+    console.log(date);
+    if (date === null || date.toString() === 'null') {
+      return 'Present';
+    } else {
+      return date.toString();
+    }
+  }
 }
